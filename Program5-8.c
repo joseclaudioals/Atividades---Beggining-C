0c5c1e92750e4d9a1c83b2a06fc37dd7bd177f33
joseclaudioals <joseclaudioals1092gmail.com>
2025-08-11 18:36:38 -0300

    Program5-8 funcional, sem condicional de casas

--- Program5-8.c
+++ Program5-8.c
@@ -1,27 +1,52 @@
 #include <stdio.h>
 
 int main(void){
-    int taboleiro[3][3]={
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9},
+    char tab[3][3]={
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'},
     };
-    unsigned int contador=8;
-    int jogador1 = 1, jogador2 = 0, escolha = 0;
+    unsigned int contador=0;
+    int escolha = 0;
 
     printf("JOGO DA VELHA\n");
 
     while(contador<9){
-        printf(" %i | %i | %i\n", taboleiro[0][0], taboleiro[1][0], taboleiro[2][0]);
+        printf(" %c | %c | %c\n", tab[0][0], tab[0][1], tab[0][2]);
         printf("===+===+===\n");
-        printf(" %i | %i | %i\n", taboleiro[0][1], taboleiro[1][1], taboleiro[2][1]);
+        printf(" %c | %c | %c\n", tab[1][0], tab[1][1], tab[1][2]);
         printf("===+===+===\n");
-        printf(" %i | %i | %i\n", taboleiro[0][2], taboleiro[1][2], taboleiro[2][2]);
-        contador++;
+        printf(" %c | %c | %c\n", tab[2][0], tab[2][1], tab[2][2]);
         
+        contador++;
+        printf("\n> Jogador %c:\n", contador%2 ? '1' : '2');
+        printf("Escolha a sua posicao: ");
+        scanf(" %i", &escolha);
+        int temp = escolha;
+        tab[(--escolha/3)][(--temp%3)]=(contador%2)? 'X' : 'O';
+        //verificando linhas iguais
+        if((tab[0][0]==tab[0][1]) && (tab[0][1]==tab[0][2]) || (tab[1][0]==tab[1][1]) && (tab[1][1]==tab[1][2]) || (tab[2][0]==tab[2][1]) && (tab[2][1]==tab[2][2])){
+            printf("o jogador %c venceu!!\n", (contador%2) ? '1' : '2');
+            break;
+        }
+            //verificando colunas iguais
+            else if((tab[0][0]==tab[1][0]) && (tab[1][0]==tab[2][0]) || (tab[0][1]==tab[1][1]) && (tab[1][1]==tab[2][1]) || (tab[0][2]==tab[1][2]) && (tab[1][2]==tab[2][2])){
+                printf("o jogador %c venceu!!\n", (contador%2) ? '1' : '2');
+                break;
+            }
+                //verificando diagonais iguais
+                else if((tab[0][0]==tab[1][1]) && (tab[1][1]==tab[2][2]) || (tab[0][2]==tab[1][1]) && (tab[1][1]&&tab[2][0])){
+                    printf("o jogador %c venceu!!\n", (contador%2) ? '1' : '2');
+                    break;
+                }
+
+        if(contador==9){
+            printf("o jogo foi empatado!\n");
+            break;
+        }
     }
     
-
+    printf("obrigado por jogar!!\n");
     
 
     return 0;
